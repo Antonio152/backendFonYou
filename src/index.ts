3191de3b281085e0b5105ba70bc01ee17b64fd07
Antonio152 <antonio152.ma@gmail.com>
2023-07-02 13:50:20 -0600

    first commit

---
+++ src/index.ts
@@ -0,0 +1,22 @@
+import express, { Application } from "express";
+import cors from "cors";
+import axios from "axios";
+import dotenv from "dotenv";
+dotenv.config();
+const app: Application = express();
+app.use(express.json());
+app.use(cors());
+const PORT = process.env.PORT ?? 3000;
+
+app.get("/api/character", async (_req, res) => {
+  try {
+    const response = await axios.get(`${process.env.API_CHARACTER}`);
+    const characters = response.data;
+    res.json(characters);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener los personajes" });
+  }
+});
+app.listen(PORT, () => {
+  console.log(`⚡ Server running on port ${PORT} ⚡`);
+});
